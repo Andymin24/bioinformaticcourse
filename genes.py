4d9b957ac9bb110ae434709015463952d61f3c6a
Andymin24 <129067917+Andymin24@users.noreply.github.com>
2023-03-29 15:14:43 +0000

    Corrigiendo el código

--- genes.py
+++ genes.py
@@ -1,10 +1,10 @@
-# def descargar(gen1.txt)
+def descargar(accs)
     from Bio import Entrez 
     from Bio import SeqIO 
 
     # Simple fasta en GenBank NCBI
     Entrez.email = "gualapuro.moises@gmail.com" 
-    with Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id="L41996") as gene1:
+    with Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id="accs") as genes:
         seq_record = SeqIO.read(handle, "fasta")
     print("%s with %i features" % (seq_record.id, len(seq_record.features)))
     seq_record.seq
