9567faaaabab0fc293456431a886e887852cdcef
Andymin24 <129067917+Andymin24@users.noreply.github.com>
2023-03-31 13:32:22 +0000

    Eliminar archivos no necesarios

--- genes.py
+++ genes.py
@@ -1,11 +1,9 @@
-def descargar(accs)
-    from Bio import Entrez 
-    from Bio import SeqIO 
-    from Bio import GenBank 
 
-    # Simple fasta en GenBank NCBI
-    Entrez.email = "gualapuro.moises@gmail.com" 
-    with Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id="accs") as genes:
-        seq_record = SeqIO.read(handle, "fasta")
-    print("%s with %i features" % (seq_record.id, len(seq_record.features)))
-    seq_record.seq
+from Bio import Entrez 
+from Bio import SeqIO 
+from Bio import GenBank 
+
+
+def descargar(accs):
+    
+
